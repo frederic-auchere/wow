95e59345c8bd0c6e43da0e6aa3eed9e04f122a51
fauchere <frederic.auchere@gmail.com>
2022-10-17 10:46:30 +0200

    Fixed clock time & added second hand

--- eui-tools/plotting.py
+++ eui-tools/plotting.py
@@ -6,10 +6,12 @@ def draw_clock(date, ax, size=0.05):
     _, _, _, h, m, s = Time(date).to_value('ymdhms')
     if h > 12:
         h -= 12
-    h = 2*np.pi*(1 - (h/12 + m/60 + s/3600))
+    h = 2*np.pi*(1 - (h + m/60 + s/3600)/12)
+    m = 2*np.pi*(1 - (m/60 + s/3600))
 
     ax = ax.inset_axes([0, 0, size, size], projection='polar')
     ax.plot((0, h), (0, 1), color='white')
+    ax.plot((0, m), (0, 0.5), color='white')
 
     for sp in ax.spines:
         ax.spines[sp].set_color('white')
