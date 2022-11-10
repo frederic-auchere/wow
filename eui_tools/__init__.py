8ece7910c110cdbac2c8fdb49861ffe7d1e2d3b7
fauchere <frederic.auchere@gmail.com>
2022-11-10 09:18:50 +0100

    Added hook to wow function

--- eui_tools/__init__.py
+++ eui_tools/__init__.py
@@ -1,1 +1,9 @@
+from watroo import utils
+
 __version__ = '0.0.1'
+__all__ = ['wow']
+
+
+def wow(image, *args, **kwargs):
+    wow_image, _ = utils.wow(image, *args, **kwargs)
+    return wow_image
